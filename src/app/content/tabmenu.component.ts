5fdda3adddfde1e8674b386498ce900ce97c548f
tgkim2018 <36981602+tgkim2018@users.noreply.github.com>
2018-03-19 09:54:36 +0900

    Add files via upload

--- src/app/content/tabmenu.component.ts
+++ src/app/content/tabmenu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../common.service';
+import { TabListForm } from '../navi-list';
 
 @Component({
   selector: 'app-tabmenu',
@@ -6,16 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabmenu.component.css']
 })
 export class TabmenuComponent implements OnInit {
-  tab_menu='{"tab_menu":"["DashBoard", "Analysis", "Configuration", "Report", "intelligence"]"}';
-  tab_menu1='DashBoard';
-  tab_menu2='Analysis';
-  tab_menu3='Configuration';
-  tab_menu4='Report';
-  tab_menu5='intelligence';
+  //tab_menu='{"tab_menu":"["DashBoard", "Analysis", "Configuration", "Report", "intelligence"]"}';
+  //tab_menu1='DashBoard';
+  //tab_menu2='Analysis';
+  //tab_menu3='Configuration';
+  //tab_menu4='Report';
+  //tab_menu5='intelligence';
+  tablist:TabListForm;
+  TabLists:TabListForm[];
+  selectedList:TabListForm;
   
-  constructor() { }
+  constructor( private commonService: CommonService ) { }
 
   ngOnInit() {
+    this.getTabLists();
   }
   
+  OnSelect(tablist): void {
+    this.selectedList = tablist;
+  }
+
+  getTabLists(): void {
+    this.commonService.getTabLists().subscribe(TabLists => this.TabLists = TabLists);
+  }
 }
